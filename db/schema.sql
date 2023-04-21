c9637b326c699d01a351462ac42b3ce2a9f2c10b
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-21 09:53:00 -0400

    added values to tables in sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,22 @@ CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
 
 CREATE TABLE departments (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     names VARCHAR(30)
 
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL
+    department_id INT FOREIGN KEY REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT,
+    role_id INT FOREIGN KEY REFERENCES roles(id)
     manager_id INT
 );
---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO departments (names)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Marketing");
+
+INSERT INTO roles (title, salary)
+VALUES ("Sales Lead", 75000),
+       ("Salesperson", 60000)
+       ("Lead Software Engineer", 120000),
+       ("Junior Software Engineer", 90000),
+       ("Law Team Lead", 115000)
+       ("Lawyer", 85000),
+       ("Marketing Lead", 70000),
+       ("Marketing Assistant", 50000),
+       ("Accountant", 100000),
+       ("Account Manager", 70000);
+
+INSERT INTO employees (first_name, last_name)
+VALUES ("John", "Doe"),
+       ("Jane", "Dee"),
+       ("Harry", "Smith"),
+       ("Caroline", "White"),
+       ("George", "Jones"),
+       ("Celia", "Williams"),
+       ("Michael", "Garcia"),
+       ("Cheryl", "Johnson"),
+       ("Gary", "Brown"),
+       ("Christina", "Lee");
