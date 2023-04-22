ff8599b027ad91e2c5f5bf29a8ceaa00c1716160
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-22 11:25:12 -0400

    added data to seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
 
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     names VARCHAR(30)
 
 );
@@ -12,14 +12,16 @@ CREATE TABLE departments (
 CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL
-    department_id INT FOREIGN KEY REFERENCES departments(id)
+    salary DECIMAL,
+    department_id INT NOT NULL, 
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT FOREIGN KEY REFERENCES roles(id)
-    manager_id INT
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,26 +5,26 @@ VALUES ("Sales"),
        ("Legal"),
        ("Marketing");
 
-INSERT INTO roles (title, salary)
-VALUES ("Sales Lead", 75000),
-       ("Salesperson", 60000)
-       ("Lead Software Engineer", 120000),
-       ("Junior Software Engineer", 90000),
-       ("Law Team Lead", 115000)
-       ("Lawyer", 85000),
-       ("Marketing Lead", 70000),
-       ("Marketing Assistant", 50000),
-       ("Accountant", 100000),
-       ("Account Manager", 70000);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 75000, 1),
+       ("Salesperson", 60000, 1),
+       ("Lead Software Engineer", 120000, 2),
+       ("Junior Software Engineer", 90000, 2),
+       ("Law Team Lead", 115000, 3),
+       ("Lawyer", 85000, 3),
+       ("Marketing Lead", 70000, 4),
+       ("Marketing Assistant", 50000, 4),
+       ("Accountant", 100000, 5),
+       ("Account Manager", 70000, 5);
 
-INSERT INTO employees (first_name, last_name)
-VALUES ("John", "Doe"),
-       ("Jane", "Dee"),
-       ("Harry", "Smith"),
-       ("Caroline", "White"),
-       ("George", "Jones"),
-       ("Celia", "Williams"),
-       ("Michael", "Garcia"),
-       ("Cheryl", "Johnson"),
-       ("Gary", "Brown"),
-       ("Christina", "Lee");
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+       ("Jane", "Dee", 2),
+       ("Harry", "Smith", 3),
+       ("Caroline", "White", 4),
+       ("George", "Jones", 5),
+       ("Celia", "Williams", 6),
+       ("Michael", "Garcia", 7),
+       ("Cheryl", "Johnson", 8),
+       ("Gary", "Brown", 9),
+       ("Christina", "Lee", 10);
