fd8a3c2da7d4b963784c6906f44b8ffa120568a5
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-22 13:27:06 -0400

    added view all employees function, still need manager id

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function viewRoles() {
 };
 
 function viewEmployees() {
-    db.query('SELECT ', (err, results) => {
+    db.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.names AS "Department Names", roles.salary FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON roles.department_id = departments.id', (err, results) => {
         if (err) {
             throw err
         }
